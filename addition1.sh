f93b5ec8e1826941d0fe34c2b10c02fbb041b718
Mourya <mouryawl@gmail.com>
2023-08-14 14:18:32 +0530

    Removed old script

--- addition1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#This script will add the numbers by taking the iputs from user
-#echo "Enter two value:"
-echo "Enter the First number:"
-read x
-echo "Enter the Second number:"
-read y
-sum=`expr $x + $y`
-echo "Addition of two number is: $sum"
